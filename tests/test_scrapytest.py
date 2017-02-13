18a1adec8a169baa16e3ea5cab4ee6958aac7f9c
coderatchet <jarednagle@gmail.com>
2017-02-13 14:06:09 +1100

    added simple tests and alias for setup.py test -> pytest

---
+++ tests/test_scrapytest.py
@@ -0,0 +1,24 @@
+#!/usr/bin/bash
+# -*- coding: utf-8 -*-
+"""
+    scrapytest.py
+
+    Copyright 2017 CodeRatchet
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+import pytest
+from scrapytest import __version_info__, __version__
+
+
+def test_version_info():
+    assert isinstance(__version_info__, tuple)
+
+
+def test_version_string():
+    assert isinstance(__version__, str)
