0f957e8b93a83d7fe85d3d64cf6e1f4480614225
coderatchet <jarednagle@gmail.com>
2017-02-13 13:38:21 +1100

    version info, setup.py populated

---
+++ scrapytest/__init__.py
@@ -0,0 +1,19 @@
+#!/usr/bin/bash
+# -*- coding: utf-8 -*-
+"""
+    __init__.py
+
+    Copyright 2017 CodeRatchet
+
+    Licensed under the Apache License, Version 2.0 (the "License");
+    you may not use this file except in compliance with the License.
+    You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+"""
+
+__version_info__ = ("0",  # Major
+                    "0",  # Minor
+                    "1")  # Patch
+
+__version__ = '.'.join(__version_info__)
--- setup.py
+++ setup.py
@@ -11,3 +11,37 @@
 
     http://www.apache.org/licenses/LICENSE-2.0
 """
+
+from setuptools import setup, find_packages
+from scrapytest import __version__
+
+setup(
+    name="scrapytest",
+    version=__version__,
+    description='A Test for assessing developer\'s proficiency',
+    url='https://github.com/coderatchet/scrapt-test.git',
+    long_description=open("README.md").read(),
+    download_url='https://github.com/coderatchet/scrapytest/archive/' + __version__ + '.tar.gz',
+    license='Apache 2.0',
+    author='CodeRatchet',
+    author_email='coderatchet@gmail.com',
+    maintainer='Jared Nagle',
+    maintainer_email='coderatchet@gmail.com',
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Environment :: Console',
+        'License :: OSI Approved',
+        'License :: OSI Approved :: Apache Software License',
+        'Natural Language :: English',
+        'Intended Audience :: Developers',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 3.5',
+        'Topic :: Text Processing :: Markup :: HTML'
+    ],
+    platforms=['any'],
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=['scrapy==1.3.1'],
+    tests_require=['pytest'],
+    setup_requires=['pytest-runner']
+)
