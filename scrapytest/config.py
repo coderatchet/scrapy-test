d9afb6aa4a6d8b619fbab78033ee76ebbf9763ca
coderatchet <jarednagle@gmail.com>
2017-02-15 13:42:10 +1100

    added environment sensitive config. set ENV variable to desired env and it will read config.<ENV>.json files merged over default config.json

--- scrapytest/config.py
+++ scrapytest/config.py
@@ -14,6 +14,21 @@
 import json
 import os
 
-config_file_path = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir, 'config.json')
-default_config_data = open(config_file_path).read()
-config = json.loads(default_config_data)
+from scrapytest.utils import ImmutableMergingDictionary
+
+root_dir = os.path.join(os.path.dirname(os.path.realpath(__file__)), os.pardir)
+config_file_path = os.path.join(root_dir, 'config.json')
+
+# load the default configuration
+with open(config_file_path) as file:
+    default_config_data = file.read()
+    config = ImmutableMergingDictionary(json.loads(default_config_data))
+
+    # load the custom environment configuration over the original
+    env = os.getenv('ENV', None)
+    env_config_file = os.path.join(root_dir, 'config.' + str(env) + '.json')
+    if env and os.path.exists(env_config_file):
+        with open(env_config_file) as env_file:
+            env_config_data = env_file.read()
+            env_config = json.loads(env_config_data)
+            config = ImmutableMergingDictionary(env_config, config)
