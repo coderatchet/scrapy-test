b411da50e93819a283dcd9ef0ed81e21af9b5dce
coderatchet <jarednagle@gmail.com>
2017-02-23 12:35:19 +1100

    Tidied main application runner. see additional comments...
    - placed parser configuration and parsing in Runner class
    - seperated crawl and search into seperate subparsers
    - each subparser share a log_parser as its parent
    - added --section, --no-save and --print-json argument to crawl (not yet
    implemented)
    - added expression positional arg to search (not yet implemented)

--- scrapytest/main.py
+++ scrapytest/main.py
@@ -10,32 +10,131 @@
 
     http://www.apache.org/licenses/LICENSE-2.0
 """
+import logging
+import os
+import sys
+from argparse import ArgumentParser
 
 from scrapy.crawler import CrawlerProcess
 
-
-def crawl():
-    from scrapy.settings import Settings
-    from scrapytest.spiders import GuardianNewsSpider
-
-    from scrapytest.config import config
-
-    settings = Settings()
-    settings.set("USER_AGENT", config['crawler_user_agent'])
-    crawler = CrawlerProcess(settings=settings)
-    # stop reactor when spider closes
-    crawler.crawl(GuardianNewsSpider)
-    crawler.start()
-    crawler.join()
+from scrapytest.config import config
 
 
-def main():
-    import argparse
-    parser = argparse.ArgumentParser()
-    args = parser.parse_args()  # type: dict
-    if '-h' not in args and '--help' not in args:
-        crawl()
+class Runner:
+    """
+    main crawler api.
+
+    usage:
+        `./run crawl [OPTIONS...]` will crawl the website and store the articles in the attached database
+        `./run search [OPTIONS...]` [EXPRESSION] will search for a given article with the following search expression.
+    """
+
+    def __init__(self):
+
+        # setup the parse and its initial values
+        self._parser = ArgumentParser(prog=os.path.basename(__file__),
+                                      description='Utility for parsing and searching articles on the '
+                                                  '\"theguardian.com.au\" website')
+        self._configure_parser()
+        if len(sys.argv) < 2:
+            self._parser.print_usage()
+            exit(1)
+        self._custom_guardian_config = {}
+        self._args = {}
+
+    def run(self):
+        # Parse command line args
+        parsed_args = self._parser.parse_args()
+        self._args = vars(parsed_args)
+
+        # configure the program according to user args
+        self._process_arguments()
+
+        # default function according to sub_parser is stored in the func attribute on the Namespace.
+        parsed_args.func()
+        self._custom_guardian_config = {}  # type: dict
+
+    def search(self):
+        """ search the database for the terms defined in the argument to --search/-s """
+        # TODO: Implement
+        pass
+
+    def crawl(self):
+        """ crawl through the database and either save the results to a database or text files. """
+        # setup settings
+        from scrapy.settings import Settings
+        from scrapytest.spiders import GuardianNewsSpider
+
+        settings = Settings()
+        settings.set("USER_AGENT", config['crawler_user_agent'])
+        settings.set('custom_guardian_config', self._custom_guardian_config)
+
+        crawler = CrawlerProcess(settings=settings)
+        crawler.crawl(GuardianNewsSpider)
+        crawler.start()
+        crawler.join()
+
+    def _process_arguments(self):
+        """ process and save the effects of command line arguments """
+        # log level
+        if 'log_level' in self._args:
+            try:
+                # convert the string to the mapped int
+                uppercase_log_level = str.upper(self._args['log_level'])
+                # noinspection PyProtectedMember
+                logging.getLogger().setLevel(logging._nameToLevel[uppercase_log_level])
+            except KeyError as key_error:
+                logging.log(logging.CRITICAL,
+                            "could not determine logging level from string: {}\n{}".format(self._args['log_level'],
+                                                                                           str(key_error)))
+                exit(1)
+        if 'max_depth' in self._args:
+            self._custom_guardian_config.update({'max_depth': self._args['max_depth']})
+        pass
+
+    def _configure_parser(self):
+        """ Add the command line options to the argument parser """
+        # All parsers have logging
+        self._log_parser = ArgumentParser(add_help=False)
+        self._log_parser.add_argument('-l', '--log-level', help="set level of log output", dest='log_level',
+                                      action='store',
+                                      type=str, default='INFO',
+                                      choices=['CRITICAL', 'ERROR', 'WARNING', 'INFO', 'DEBUG'])
+
+        self._subparsers = self._parser.add_subparsers()
+
+        # setup search api
+        self._search_parser = self._subparsers.add_parser('search', help='Search for articles in the database',
+                                                          parents=[self._log_parser])  # type: ArgumentParser
+        self._search_parser.set_defaults(func=self.search)
+
+        # TODO: Implement functionality
+        self._search_parser.add_argument('expression', help="search query for finding an article", nargs=1,
+                                         action='store')
+
+        # setup crawl api
+        self._crawl_parser = self._subparsers.add_parser('crawl',
+                                                         help='Crawl through \"theguardian.com.au\" and optionally '
+                                                              'store data',
+                                                         parents=[self._log_parser])  # type: ArgumentParser
+        self._crawl_parser.set_defaults(func=self.crawl)
+        self._crawl_parser.add_argument('--section', help="section to search under", dest='section', nargs='+',
+                                        choices=config['guardian_spider']['collection_paths'])
+
+        # TODO: Implement functionality
+        self._crawl_parser.add_argument('--no-save', help="do not save results to the database",
+                                        dest='save_crawl_results',
+                                        action='store_false', default=True)
+
+        # TODO: Implement functionality
+        self._crawl_parser.add_argument('-p', '--print-json', help="print results in json format", action='store_true',
+                                        dest='print_json')
+
 
 if __name__ == '__main__':
-    main()
-    crawl()
+    """ see above """
+    runner = Runner()
+    try:
+        runner.run()
+    except Exception as e:
+        logging.log(logging.ERROR, str(e))
