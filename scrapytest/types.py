4745af3cc1c7618d606825147cb46eb95c8e6960
coderatchet <jarednagle@gmail.com>
2017-02-24 18:38:14 +1100

    Added indexes for searchable text fields in Article meta data

--- scrapytest/types.py
+++ scrapytest/types.py
@@ -1,8 +1,15 @@
 from mongoengine import Document, StringField, DateTimeField
 
+
 class Article(Document):
     """ Document Object for storing news article data. """
-    meta = {'db_alias': 'default'}
+    meta = {'db_alias': 'default',
+            'indexes': [
+                {'fields': ['$title', "$content"],
+                 'default_language': 'english',
+                 'weights': {'title': 10, 'content': 2}
+                 }
+            ]}
 
     title = StringField(required=True)
     content = StringField(required=True)
